886148e09261b4827f567818d030b1cc7ef875e8
Nicholas Edwards <edwardsnjd@googlemail.com>
2024-10-15 21:50:55 +0800

    feat: Accept filename as optional first argument

--- chat2txt.py
+++ chat2txt.py
@@ -1,14 +1,17 @@
 #! /usr/bin/env python3
 
-# USAGE: ./chat2txt.py < cha_file
+# USAGE:
+#   ./chat2txt.py cha_file
+#   ./chat2txt.py < cha_file
 
 import re
 import sys
 
 body_line_regex = re.compile(r'^\*PAR\d+:  (.*) \. \S+_\S+$')
 
-for line in sys.stdin:
-    utterance_match = body_line_regex.match(line)
-    if utterance_match:
-        text = utterance_match.group(1)
-        print(text)
+with open(sys.argv[1], 'r') if len(sys.argv) > 1 else sys.stdin as file:
+    for line in file:
+        utterance_match = body_line_regex.match(line)
+        if utterance_match:
+            text = utterance_match.group(1)
+            print(text)
